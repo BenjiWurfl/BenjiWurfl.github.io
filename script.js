ec3036d797d70f5d1b611706b427890967aaffd5
Benjamin Würflinger <benjamin.wuerflinger@htl-wels.at>
2023-12-28 11:36:16 +0100

    Recover old Calendar

--- script.js
+++ script.js
@@ -1,3 +1,22 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.0/firebase-app.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.7.0/firebase-firestore.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.7.0/firebase-auth.js";
+  
+const firebaseConfig = {
+  apiKey: "AIzaSyBe7d9bllq8RnmI6xxEBk3oub3qogPT2aM",
+  authDomain: "thinkwise-c7673.firebaseapp.com",
+  databaseURL: "https://thinkwise-c7673-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "thinkwise-c7673",
+  storageBucket: "thinkwise-c7673.appspot.com",
+  messagingSenderId: "37732571551",
+  appId: "1:37732571551:web:9b90a849ac5454f33a85aa",
+  measurementId: "G-8957WM4SB7"
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+const auth = getAuth(app);
+
 const calendar = document.querySelector(".calendar"),
   date = document.querySelector(".date"),
   daysContainer = document.querySelector(".days"),
