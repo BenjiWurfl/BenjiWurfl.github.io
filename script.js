d927e88e0ba3568995b653a7e664a4b5d4f8dbab
Benjamin Würflinger <benjamin.wuerflinger@htl-wels.at>
2024-02-13 11:48:51 +0100

    Adding navbar

--- script.js
+++ script.js
@@ -607,3 +607,32 @@ function editEventInFirestore(eventId, updatedEvent) {
       console.error("Error updating event: ", error);
     });
 }
+
+//Navbar
+
+const body = document.querySelector('body'),
+      sidebar = body.querySelector('nav'),
+      toggle = body.querySelector(".toggle"),
+      searchBtn = body.querySelector(".search-box"),
+      modeSwitch = body.querySelector(".toggle-switch"),
+      modeText = body.querySelector(".mode-text");
+
+
+toggle.addEventListener("click" , () =>{
+    sidebar.classList.toggle("close");
+})
+
+searchBtn.addEventListener("click" , () =>{
+    sidebar.classList.remove("close");
+})
+
+modeSwitch.addEventListener("click" , () =>{
+    body.classList.toggle("dark");
+    
+    if(body.classList.contains("dark")){
+        modeText.innerText = "Light mode";
+    }else{
+        modeText.innerText = "Dark mode";
+        
+    }
+});
